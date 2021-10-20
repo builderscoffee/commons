0a132bed8070b5a0e41d9d5d826247a637473ec6
GameurDuXVI <gameur.codeur@gmail.com>
2021-10-20 15:52:01 +0200

    Removes password in-code

--- src/main/java/eu/builderscoffee/commons/common/configuration/settings/RedisConfig.java
+++ src/main/java/eu/builderscoffee/commons/common/configuration/settings/RedisConfig.java
@@ -7,8 +7,6 @@ import org.bukkit.Bukkit;
 @Data
 public class RedisConfig {
 
-    private String clientName = "test";
-    private String ip = "54.36.124.50";
-    private String password = "LbWKFC4gcTa5cepd2siPk2ifibVVGGfqDR2aPp7ZTVYF3gxvn9SwxJ5eFTXanxr2";
+    private String clientName, ip, password = "";
     private int port = 6379;
 }
